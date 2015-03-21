24f082e7a9641dfdda013bb59a435eaa14a0d117
Chintan Shah <chintan@wohlig.com>
2015-03-21 18:52:33 +0530

    git a git a

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");//Dont cache
+    header("Pragma: no-cache");//Dont cache
+    header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");//Make sure it expired in the past (this can be overkill)
+
+
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
