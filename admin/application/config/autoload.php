7b3a37937c1ae4d8b4c88af932b19a95516d04a8
Chintan Shah <chintan@wohlig.com>
2015-03-31 20:12:29 +0530

    add chintan table

--- admin/application/config/autoload.php
+++ admin/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','session','form_validation','cart','csvreader','MY_Session');
+$autoload['libraries'] = array('database','session','form_validation','cart','csvreader','MY_Session','chintantable');
 
 
 /*
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('user_model','category_model','wishlist_model','cart_model','product_model','order_model','page_model','slider_model','banner_model','celebcorner_model','bloggerscorner_model','navigation_model','currency_model','paymentgateway_model','discountcoupon_model','newsletter_model','pickofweak_model');
+$autoload['model'] = array('user_model','category_model','wishlist_model','cart_model','product_model','order_model','page_model','slider_model','banner_model','celebcorner_model','bloggerscorner_model','navigation_model','currency_model','paymentgateway_model','discountcoupon_model','newsletter_model','pickofweak_model','productimage_model');
 
 /* End of file autoload.php */
 /* Location: ./application/config/autoload.php */
--- admin/application/libraries/MY_Session.php
+++ admin/application/libraries/MY_Session.php
@@ -12,10 +12,10 @@ class MY_Session extends CI_Session
     function sess_update()
     {
        // skip the session update if this is an AJAX call!
-       if ( !IS_AJAX )
-       {
-           parent::sess_update();
-       }
+//       if ( !IS_AJAX )
+//       {
+//           parent::sess_update();
+//       }
     } 
 
 }
