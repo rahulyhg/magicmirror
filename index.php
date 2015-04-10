511ecd3da9d3c086cdf5907623cf20544eaa333f
Chintan Shah <chintan@wohlig.com>
2015-04-10 13:42:30 +0530

    favicon

--- index.php
+++ index.php
@@ -41,8 +41,23 @@
         }
     </script>
 
-
-    <link rel="shortcut icon" href="img/fashionlg.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="img/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="img/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+    <link rel="manifest" href="img/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="img/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
     <link href="lib/css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="lib/css/font-awesome.min.css" rel="stylesheet">
     <link href="lib/range/angular.rangeSlider.css" rel="stylesheet">
@@ -53,10 +68,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <script src="lib/js/jquery-1.11.1.min.js"></script>
     <script src="lib/js/angular.min.js"></script>
-<!--    <script src="lib/js/angular-animate.js"></script>-->
+    <!--    <script src="lib/js/angular-animate.js"></script>-->
     <script src="lib/js/angular-route.min.js"></script>
-<!--    <script src="lib/js/angular-resource.min.js"></script>-->
-<!--
+    <!--    <script src="lib/js/angular-resource.min.js"></script>-->
+    <!--
     <script src="lib/js/bootstrap.min.js"></script>
     <script src="lib/js/ui-bootstrap-tpls-0.11.0.min.js"></script>
 -->
@@ -71,7 +86,7 @@
 </head>
 
 <body>
-    
+
     <div>
         <div ng-view></div>
     </div>
