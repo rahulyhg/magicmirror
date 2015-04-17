5928401e137329a015e3d055047d40cb717714ef
Chintan Shah <chintan@wohlig.com>
2015-04-17 19:03:15 +0530

    social log

--- admin/application/controllers/login.php
+++ admin/application/controllers/login.php
@@ -5,6 +5,7 @@ class Login extends CI_Controller
 {
 	public function index( )
 	{
+        $this->session->sess_destroy();
 		$is_logged_in = $this->session->userdata( 'logged_in' );
 		if ( $is_logged_in == 'true' ) {
 			redirect( base_url() . 'index.php/site', 'refresh' );
