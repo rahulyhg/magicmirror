444be9207a724aaf5764cb3013cda5c6e073a6fc
jagruti <patiljagruti181@gmail.com>
2015-05-07 14:14:46 +0530

    Revert "cart popup"
    This reverts commit 5867ae0ef0a36abab08cdb2c677579c761f7fed2.

--- js/app.js
+++ js/app.js
@@ -146,11 +146,6 @@ firstapp.config(['$routeProvider', '$locationProvider',
             templateUrl: 'views/template.html',
             controller: 'myaccount'
         }).
-        
-          when('/emailclick', {
-            templateUrl: 'views/emailclick.html',
-            controller: 'product'
-        }).
         when('/productcare', {
             templateUrl: 'views/template.html',
             controller: 'productcare'
@@ -240,6 +235,49 @@ firstapp.filter('convertprice', function() {
         }
     };
 });
+
+firstapp.directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' + 
+          '<div class="modal-dialog">' + 
+            '<div class="modal-content">' + 
+              '<div class="modal-header">' + 
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                '<h4 class="modal-title">{{ title }}</h4>' + 
+              '</div>' + 
+              '<div class="modal-body" ng-transclude></div>' + 
+            '</div>' + 
+          '</div>' + 
+        '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
+
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+  });
+
 
 var formvalidation = function(allvalidation) {
     var isvalid2 = true;
--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-var phonecatControllers = angular.module('phonecatControllers', ['templateservicemod', 'Service', 'ngRoute','ngDialog']);
+var phonecatControllers = angular.module('phonecatControllers', ['templateservicemod', 'Service', 'ngRoute']);
 phonecatControllers.controller('home',
     function($scope, TemplateService, MainJson, $rootScope, $location) {
         ////$scope.firstloadclass = TemplateService.firstload;
@@ -1794,7 +1794,7 @@ phonecatControllers.controller('category',
         $scope.template = TemplateService;
         TemplateService.header = "views/header.html";
         TemplateService.navigation = "views/navigation.html";
-        TemplateService.f = "views/category.html";
+        TemplateService.content = "views/category.html";
         TemplateService.slider = "";
         //    console.log($routeParams.CategoryId);
         //        if ($routeParams.CategoryId == "3") {
@@ -2002,7 +2002,7 @@ phonecatControllers.controller('category',
     });
 
 phonecatControllers.controller('product',
-    function($scope, $routeParams, TemplateService, MainJson, $timeout, $location , ngDialog ) {
+    function($scope, $routeParams, TemplateService, MainJson, $timeout, $location) {
         //$scope.firstloadclass = TemplateService.firstload;
         $scope.template = TemplateService;
         TemplateService.header = "views/header.html";
@@ -2016,20 +2016,13 @@ phonecatControllers.controller('product',
         $scope.addedtocart = "hide";
         $scope.loginlogouttext = "Login";
 
-    
-    //popup
-    
-         $scope.onemailclick = function(listing) {
-            console.log(listing);
-            $.jStorage.set("listingid", listing);
-            ngDialog.open({
-                template: 'views/emailclick.html',
-                controller: 'product'
-            });
-        };
-    //
 
-        
+        //  MODAL POPUP
+        $scope.showModal = false;
+    $scope.toggleModal = function(){
+        $scope.showModal = !$scope.showModal;
+    };
+    
     
     
         //authenticate
@@ -2097,11 +2090,16 @@ phonecatControllers.controller('product',
             TemplateService.cartclicked = "animated swing";
 
             MainJson.addtocart(id, name, price, quantity).success(cartt);
-            ngDialog.open({
-                template: 'views/emailclick.html',
-                controller: 'product'
-            });
-//            $scope.addedtocart = "show";
+//            ModalService.showModal({
+//                    templateUrl: 'popup.html',
+//                    controller: "home"
+//                }).then(function (modal) {
+//                    modal.element.modal();
+//                    modal.close.then(function (result) {
+//                        $scope.message = "You said " + result;
+//                    });
+//                });
+            $scope.addedtocart = "show";
         };
         var addedtowaitinglist = function(data) {
             console.log(data);
