9da63aee9484498e88a6771f1abdacfc7b7a671c
sohan1 <shn619@gmail.com>
2015-05-21 16:51:40 +0530

    changes

--- index.php
+++ index.php
@@ -95,6 +95,16 @@
     <script src="lib/range/angular.rangeSlider.js "></script>
     <script src="lib/js/ng-infinite-scroll.min.js "></script>
     <!--  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>-->
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47309010-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 
 <body>
